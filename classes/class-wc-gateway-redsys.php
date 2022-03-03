ce779df82a52ffbaf76871cd5204e6a910bf1131
José Conti <j.conti@joseconti.com>
2022-03-03 14:58:23 +0100

    update

--- classes/class-wc-gateway-redsys.php
+++ classes/class-wc-gateway-redsys.php
@@ -612,7 +612,7 @@ class WC_Gateway_Redsys extends WC_Payment_Gateway {
 			header( 'HTTP/1.1 200 OK' );
 			do_action( 'valid_redsys_standard_ipn_request', $_POST );
 		} else {
-			wp_die( 'Servired/RedSys Notification Request Failure' );
+			wp_die( 'Do not access this page directly' );
 		}
 	}
 	/**
