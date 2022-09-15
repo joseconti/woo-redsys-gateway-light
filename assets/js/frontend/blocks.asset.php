8d2f38b48cf693f6d2005dc986d1a25aa70f7143
José Conti <j.conti@joseconti.com>
2022-09-15 22:23:50 +0200

    update

--- assets/js/frontend/blocks.asset.php
+++ assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '6e8773de92cc51599225fe7f511cf7d5');
+<?php return array('dependencies' => array('wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '3dd79794711bed069687ca8c5d0ceb1e');
