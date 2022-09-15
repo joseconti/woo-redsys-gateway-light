9d6748e9b405dc22e6e9e7e2395c17b330ff6ed2
José Conti <j.conti@joseconti.com>
2022-09-15 22:18:53 +0200

    Update index.js

--- resources/js/frontend/index.js
+++ resources/js/frontend/index.js
@@ -4,16 +4,16 @@ import { registerPaymentMethod } from '@woocommerce/blocks-registry';
 import { decodeEntities } from '@wordpress/html-entities';
 import { getSetting } from '@woocommerce/settings';
 
-const settings = getSetting( 'redsys_data', {} );
-const settingsbizum = getSetting( 'bizumredsys_data', {} );
+const settings            = getSetting( 'redsys_data', {} );
+const settingsbizumredsys = getSetting( 'bizumredsys_data', {} );
 
 const defaultLabel = __(
 	'WooCommerce Redsys Gateway Ligh',
 	'woo-gutenberg-products-block'
 );
 
-const label = decodeEntities( settings.title ) || defaultLabel;
-const labelbizum = decodeEntities( settingsbizum.title ) || defaultLabel;
+const label      = decodeEntities( settings.title ) || defaultLabel;
+const labelbizum = decodeEntities( settingsbizumredsys.title ) || defaultLabel;
 /**
  * Content component
  */
@@ -21,7 +21,7 @@ const Content = () => {
 	return decodeEntities( settings.description || '' );
 };
 const Contentbizum = () => {
-	return decodeEntities( settingsbizum.description || '' );
+	return decodeEntities( settingsbizumredsys.description || '' );
 };
 /**
  * Label component
@@ -59,7 +59,7 @@ const Bizum = {
 	canMakePayment: () => true,
 	ariaLabel: labelbizum,
 	supports: {
-		features: settingsbizum.supports,
+		features: settingsbizumredsys.supports,
 	},
 };
 
