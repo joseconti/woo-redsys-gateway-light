fcb870ffbda2bc64519526ce026ec54ac53f5432
José Conti <j.conti@joseconti.com>
2022-12-30 19:17:03 +0100

    update

--- classes/class-wc-gateway-bizum-redsys.php
+++ classes/class-wc-gateway-bizum-redsys.php
@@ -632,13 +632,17 @@ class WC_Gateway_Bizum_Redsys extends WC_Payment_Gateway {
 				cursor:			"wait",
 				lineHeight:		"32px"
 			}
-		});
-		jQuery("#submit_redsys_payment_form").click();'
+		});'
 		);
 		return '<form action="' . esc_url( $redsys_adr ) . '" method="post" id="redsys_payment_form" target="_top">
 		' . implode( '', $form_inputs ) . '
 		<input type="submit" class="button-alt" id="submit_redsys_payment_form" value="' . __( 'Pay with Bizum', 'woo-redsys-gateway-light' ) . '" /> <a class="button cancel" href="' . esc_url( $order->get_cancel_order_url() ) . '">' . __( 'Cancel order &amp; restore cart', 'woo-redsys-gateway-light' ) . '</a>
-	</form>';
+	</form><
+	script>
+	$(document).ready(function() {
+	   jQuery("#submit_redsys_payment_form").click();
+	 });
+  </script>';
 	}
 
 	/**
