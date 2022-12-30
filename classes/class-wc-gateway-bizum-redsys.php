2ffd94feae4744b62673f5ca6b7e40911955f54a
José Conti <j.conti@joseconti.com>
2022-12-30 19:19:31 +0100

    Update class-wc-gateway-bizum-redsys.php

--- classes/class-wc-gateway-bizum-redsys.php
+++ classes/class-wc-gateway-bizum-redsys.php
@@ -637,7 +637,7 @@ class WC_Gateway_Bizum_Redsys extends WC_Payment_Gateway {
 		return '<form action="' . esc_url( $redsys_adr ) . '" method="post" id="redsys_payment_form" target="_top">
 		' . implode( '', $form_inputs ) . '
 		<input type="submit" class="button-alt" id="submit_redsys_payment_form" value="' . __( 'Pay with Bizum', 'woo-redsys-gateway-light' ) . '" /> <a class="button cancel" href="' . esc_url( $order->get_cancel_order_url() ) . '">' . __( 'Cancel order &amp; restore cart', 'woo-redsys-gateway-light' ) . '</a>
-	</form><
+	</form>
 	<script>
 	$(document).ready(function() {
 	   jQuery("#submit_redsys_payment_form").click();
