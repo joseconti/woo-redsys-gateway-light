72ed6a87e93a31c039021b82b47574f7b313c7f5
José Conti <j.conti@joseconti.com>
2023-06-07 21:29:33 +0200

    actualizado .gitattributes

--- woocommerce-redsys.php
+++ woocommerce-redsys.php
@@ -7,7 +7,7 @@
  * Plugin Name: WooCommerce Redsys Gateway Light
  * Plugin URI: https://wordpress.org/plugins/woo-redsys-gateway-light/
  * Description: Extends WooCommerce with a RedSys gateway. This is a Lite version, if you want many more, check the premium version https://woocommerce.com/products/redsys-gateway/
- * Version: 5.1.0
+ * Version: 5.2.0
  * Author: José Conti
  * Author URI: https://www.joseconti.com/
  * Tested up to: 6.2
@@ -20,7 +20,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-define( 'REDSYS_WOOCOMMERCE_VERSION', '5.1.0' );
+define( 'REDSYS_WOOCOMMERCE_VERSION', '5.2.0' );
 define( 'REDSYS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 if ( ! defined( 'REDSYS_PLUGIN_PATH' ) ) {
 	define( 'REDSYS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
