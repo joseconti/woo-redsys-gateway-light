6bc7fc397b8df8c8b09d3804a6ae29f5a196c06c
José Conti <j.conti@joseconti.com>
2025-04-12 18:46:29 +0200

    Update woocommerce-redsys.php

--- woocommerce-redsys.php
+++ woocommerce-redsys.php
@@ -26,7 +26,7 @@ define( 'REDSYS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 if ( ! defined( 'REDSYS_PLUGIN_PATH' ) ) {
 	define( 'REDSYS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 }
-define( 'REDSYS_POST_UPDATE_URL', 'https://plugins.joseconti.com/2025/03/15/woocommerce-redsys-gateway-light-6-4-x/' );
+define( 'REDSYS_POST_UPDATE_URL', 'https://plugins.joseconti.com/2025/04/12/woocommerce-redsys-gateway-light-6-4-x/' );
 define( 'REDSYS_TELEGRAM_URL', 'https://t.me/wooredsys' );
 define( 'REDSYS_REVIEW', 'https://wordpress.org/support/plugin/woo-redsys-gateway-light/reviews/?rate=5#new-post' );
 define( 'REDSYS_DONATION', 'https://plugins.joseconti.com/product-category/plugins/donaciones/' );
